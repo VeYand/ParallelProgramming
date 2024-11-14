19f09b92a33611fd2e38b32533320d075a5826d4
VeYand <ValerikLerik17@gmail.com>
2024-11-14 12:03:51 +0300

    Поправил

--- lab5/main_1.cpp
+++ lab5/main_1.cpp
@@ -5,21 +5,17 @@
 CRITICAL_SECTION FileLockingCriticalSection;
 
 int ReadFromFile() {
-    EnterCriticalSection(&FileLockingCriticalSection);
     std::fstream myfile("balance.txt", std::ios_base::in);
     int result = 0;
     myfile >> result;
     myfile.close();
-    LeaveCriticalSection(&FileLockingCriticalSection);
     return result;
 }
 
 void WriteToFile(int data) {
-    EnterCriticalSection(&FileLockingCriticalSection);
     std::fstream myfile("balance.txt", std::ios_base::out);
     myfile << data << std::endl;
     myfile.close();
-    LeaveCriticalSection(&FileLockingCriticalSection);
 }
 
 int GetBalance() {
@@ -34,35 +30,40 @@ void Deposit(int money) {
 }
 
 void Withdraw(int money) {
-    int balance = GetBalance();
-    Sleep(20);
-    if (balance < money) {
+    if (GetBalance() < money) {
         printf("Cannot withdraw money, balance lower than %d\n", money);
-    } else {
-        balance -= money;
-        WriteToFile(balance);
-        printf("Balance after withdraw: %d\n", balance);
+        return;
     }
+
+    Sleep(20);
+    int balance = GetBalance();
+    balance -= money;
+    WriteToFile(balance);
+    printf("Balance after withdraw: %d\n", balance);
 }
 
 DWORD WINAPI DoDeposit(CONST LPVOID lpParameter) {
+    EnterCriticalSection(&FileLockingCriticalSection);
     Deposit(static_cast<int>(reinterpret_cast<intptr_t>(lpParameter)));
+    LeaveCriticalSection(&FileLockingCriticalSection);
     ExitThread(0);
 }
 
 DWORD WINAPI DoWithdraw(CONST LPVOID lpParameter) {
+    EnterCriticalSection(&FileLockingCriticalSection);
     Withdraw(static_cast<int>(reinterpret_cast<intptr_t>(lpParameter)));
+    LeaveCriticalSection(&FileLockingCriticalSection);
     ExitThread(0);
 }
 
 int main() {
-    HANDLE handles[50];
+    HANDLE handles[500];
     InitializeCriticalSection(&FileLockingCriticalSection);
     WriteToFile(0);
 
     SetProcessAffinityMask(GetCurrentProcess(), 1);
 
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 500; i++) {
         handles[i] = (i % 2 == 0)
                          ? CreateThread(NULL, 0, &DoDeposit, reinterpret_cast<LPVOID>(230), CREATE_SUSPENDED, NULL)
                          : CreateThread(NULL, 0, &DoWithdraw, reinterpret_cast<LPVOID>(1000), CREATE_SUSPENDED, NULL);
