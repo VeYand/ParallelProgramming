3bb2ff9b687d30afcb6878fbc49452475af9fcd2
VeYand <ValerikLerik17@gmail.com>
2024-12-07 20:15:44 +0300

    Реализовал 7-ю лабораторную

---
+++ lab7-1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lab7;
+
+internal static class Program
+{
+    private static async Task Main()
+    {
+        Console.Write("Введите путь к текстовому файлу: ");
+        var filePath = Console.ReadLine();
+
+        Console.Write("Введите символы для удаления (без пробелов): ");
+        var charsToRemove = Console.ReadLine();
+
+        try
+        {
+            var content = await File.ReadAllTextAsync(filePath ?? "");
+
+            var result = new string(content.Where(c => !charsToRemove?.Contains(c) ?? true).ToArray());
+
+            await File.WriteAllTextAsync(filePath ?? "", result);
+
+            Console.WriteLine("Символы успешно удалены и файл сохранен.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ lab7-2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+
+namespace lab7_2;
+
+internal static class Program
+{
+    private const string Url = "https://dog.ceo/api/breeds/image/random";
+    private const int NumberOfRequests = 50; // Specify the number of requests here
+
+    private static async Task Main()
+    {
+        SyncRequests();
+        await AsyncRequests();
+    }
+
+    private static void SyncRequests()
+    {
+        using var client = new HttpClient();
+        var stopwatch = Stopwatch.StartNew();
+
+        for (var i = 0; i < NumberOfRequests; i++)
+        {
+            var response = client.GetAsync(Url).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                continue;
+            }
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseString);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Synchronous requests completed in: {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    private static async Task AsyncRequests()
+    {
+        using var client = new HttpClient();
+        var stopwatch = Stopwatch.StartNew();
+        
+        var tasks = new Task<HttpResponseMessage>[NumberOfRequests];
+
+        for (var i = 0; i < NumberOfRequests; i++)
+        {
+            tasks[i] = client.GetAsync(Url);
+        }
+
+        var responses = await Task.WhenAll(tasks);
+
+        foreach (var response in responses)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                continue;
+            }
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Asynchronous requests completed in: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
